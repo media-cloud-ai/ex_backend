eb4b6bc45c2e303ad6722828f621d7406ce4bfc7
Marc-Antoine Arnaud <marc.antoine@nomalab.com>
2018-01-29 11:25:56 +0100

    user styling per component

---
+++ assets/src/app/app.component.ts
@@ -0,0 +1,12 @@
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-component',
+    templateUrl: 'app.component.html',
+    styleUrls: ['./app.component.less'],
+})
+
+export class AppComponent {
+  constructor() {}
+}
