a5b8aefba771cdc5a92eb5ce253543f4539b6d69
Marc-Antoine Arnaud <marc.antoine@nomalab.com>
2018-02-23 08:13:17 +0100

    display AMQP monitoring

---
+++ assets/src/app/services/amqp.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { catchError, tap } from 'rxjs/operators';
+
+import {QueuePage} from './queue_page';
+
+@Injectable()
+export class AmqpService {
+  private queuesUrl = 'api/amqp/queues';
+
+  constructor(private http: HttpClient) { }
+
+  getQueues(): Observable<QueuePage> {
+    return this.http.get<QueuePage>(this.queuesUrl)
+      .pipe(
+        tap(queuepage => this.log('fetched QueuePage')),
+        catchError(this.handleError('getQueues', undefined))
+      );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+
+  private log(message: string) {
+    console.log('AmqpService: ' + message);
+  }
+}
---
+++ assets/src/app/services/queue.ts
@@ -0,0 +1,8 @@
+
+export class Queue {
+  name: string;
+  state: string;
+  messages: number;
+  messages_ready: number;
+  consumers: number;
+}
