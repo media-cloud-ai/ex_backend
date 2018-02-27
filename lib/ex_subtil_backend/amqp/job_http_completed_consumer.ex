87e752fd08c09a387efb129e50164349f45dc3de
Marc-Antoine Arnaud <marc.antoine@nomalab.com>
2018-02-27 10:17:40 +0100

    ading new emitter and consumers for HTTP jobs

---
+++ lib/ex_subtil_backend/amqp/job_http_completed_consumer.ex
@@ -0,0 +1,19 @@
+
+defmodule ExSubtilBackend.Amqp.JobHttpCompletedConsumer do
+  require Logger
+
+  alias ExSubtilBackend.Jobs
+
+  use ExSubtilBackend.Amqp.CommonConsumer, %{
+    queue: "job_http_completed",
+    consumer: &ExSubtilBackend.Amqp.JobHttpCompletedConsumer.consume/4,
+  }
+
+  def consume(channel, tag, _redelivered, %{"job_id" => job_id, "status" => status} = payload) do
+    Logger.warn "receive #{inspect payload}"
+    Jobs.Status.set_job_status(job_id, status)
+
+    ExSubtilBackend.WorkflowStepManager.check_step_status(%{job_id: job_id})
+    Basic.ack channel, tag
+  end
+end
