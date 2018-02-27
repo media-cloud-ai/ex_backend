0369a42a1809593c930184638a6730027937401e
Marc-Antoine Arnaud <marc.antoine@nomalab.com>
2018-02-27 14:05:15 +0100

    fix dependency version

--- mix.exs
+++ mix.exs
@@ -49,8 +49,7 @@ defmodule ExSubtilBackend.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:phoenix_pubsub, "~> 1.0"},
       {:postgrex, ">= 0.0.0"},
-      # {:ex_video_factory, "0.3.2"},
-      {:ex_video_factory, path: "../ex_video_factory"},
+      {:ex_video_factory, "0.3.3"},
       {:httpotion, "~> 3.1.0"},
       {:poison, "~> 3.1"},
     ]
