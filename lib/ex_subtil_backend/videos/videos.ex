9dbb3aa5700bd18023b10af28e0f84f38b6b3410
Marc-Antoine Arnaud <marc.antoine@nomalab.com>
2018-03-12 18:23:28 +0100

    try/catch around manifest URL getter

--- lib/ex_subtil_backend/videos/videos.ex
+++ lib/ex_subtil_backend/videos/videos.ex
@@ -3,6 +3,8 @@ defmodule ExSubtilBackend.Videos do
   The Videos context.
   """
 
+  require Logger
+
   import Ecto.Query, warn: false
   alias ExSubtilBackend.Repo
   alias ExSubtilBackend.Workflows.Workflow
@@ -16,21 +18,27 @@ defmodule ExSubtilBackend.Videos do
     case workflow do
       nil -> nil
       workflow ->
-        query =
-          from item in Workflow,
-          where: item.id == ^workflow.id,
-          join: job in assoc(item, :jobs),
-          where: job.name == "upload_ftp",
-          where: fragment("?->?->>? LIKE ?", job.params, "destination", "path", "%.mpd"),
-          select: job,
-          order_by: [desc: :inserted_at],
-          limit: 1
+        try do
+          query =
+            from item in Workflow,
+            where: item.id == ^workflow.id,
+            join: job in assoc(item, :jobs),
+            where: job.name == "upload_ftp",
+            where: fragment("?->?->>? LIKE ?", job.params, "destination", "path", "%.mpd"),
+            select: job,
+            order_by: [desc: :inserted_at],
+            limit: 1
 
-        Repo.one!(query)
-        |> Map.get(:params)
-        |> Map.get("destination")
-        |> Map.get("path")
-        |> String.replace("/421959/prod/innovation/", "http://videos-pmd.francetv.fr/innovation/")
+          Repo.one!(query)
+          |> Map.get(:params)
+          |> Map.get("destination")
+          |> Map.get("path")
+          |> String.replace("/421959/prod/innovation/", "http://videos-pmd.francetv.fr/innovation/")
+        rescue
+          e ->
+            Logger.error "unable to retrieve manifest URL for #{workflow.id}: #{inspect e}"
+            nil
+        end
     end
   end
 end
