b6dc21cb75616e4cff00641ae3cfd7fb1b8f7595
Marc-Antoine Arnaud <marc.antoine@nomalab.com>
2018-03-20 14:24:24 +0100

    set job in error if needed

--- lib/ex_subtil_backend/amqp/job_ftp_error_consumer.ex
+++ lib/ex_subtil_backend/amqp/job_ftp_error_consumer.ex
@@ -2,14 +2,16 @@
 defmodule ExSubtilBackend.Amqp.JobFtpErrorConsumer do
   require Logger
 
+  alias ExSubtilBackend.Jobs.Status
+
   use ExSubtilBackend.Amqp.CommonConsumer, %{
     queue: "job_ftp_error",
     consumer: &ExSubtilBackend.Amqp.JobFtpErrorConsumer.consume/4,
   }
 
-  def consume(channel, tag, _redelivered, payload) do
-    # ExSubtilBackend.Jobs.Status.set_job_status(job_id, status)
+  def consume(channel, tag, _redelivered, %{"job_id" => job_id} = payload) do
     Logger.error "FTP error #{inspect payload}"
+    Status.set_job_status(job_id, "error")
     Basic.ack channel, tag
   end
 end
--- lib/ex_subtil_backend/amqp/job_http_error_consumer.ex
+++ lib/ex_subtil_backend/amqp/job_http_error_consumer.ex
@@ -2,13 +2,16 @@
 defmodule ExSubtilBackend.Amqp.JobHttpErrorConsumer do
   require Logger
 
+  alias ExSubtilBackend.Jobs.Status
+
   use ExSubtilBackend.Amqp.CommonConsumer, %{
     queue: "job_http_error",
     consumer: &ExSubtilBackend.Amqp.JobHttpErrorConsumer.consume/4,
   }
 
-  def consume(channel, tag, _redelivered, payload) do
+  def consume(channel, tag, _redelivered, %{"job_id" => job_id} = payload) do
     Logger.error "HTTP error #{inspect payload}"
+    Status.set_job_status(job_id, "error")
     Basic.ack channel, tag
   end
 end
