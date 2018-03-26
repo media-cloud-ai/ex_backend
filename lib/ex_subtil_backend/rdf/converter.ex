b777d87fda8c17a1f7554ebbc0f45478a4ca1297
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-03-26 12:22:10 +0200

    add routes to get and ingest RDF

---
+++ lib/ex_subtil_backend/rdf/converter.ex
@@ -0,0 +1,36 @@
+defmodule ExSubtilBackend.Rdf.Converter do
+  @moduledoc """
+  The RDF Converter context.
+  """
+
+  require Logger
+
+  defp port_format(port) when is_integer(port) do
+    Integer.to_string(port)
+  end
+  defp port_format(port) do
+    port
+  end
+
+  def get_rdf(video_id) do
+    params = %{
+      "qid" => video_id
+    }
+
+    information =
+      ExVideoFactory.videos(params)
+      |> Map.get(:videos)
+      |> List.first
+
+    config = Application.get_env(:ex_subtil_backend, :rdf_converter)
+
+    hostname = System.get_env("RDF_CONVERTER_HOSTNAME") || Keyword.get(config, :hostname, "")
+    port =
+      System.get_env("RDF_CONVERTER_PORT") || Keyword.get(config, :port, "")
+      |> port_format
+
+    url = "http://" <> hostname <> ":" <> port <> "/convert"
+
+    HTTPotion.post(url, [body: information |> Poison.encode!]).body
+  end
+end
---
+++ lib/ex_subtil_backend/rdf/perfect_memory.ex
@@ -0,0 +1,35 @@
+defmodule ExSubtilBackend.Rdf.PerfectMemory do
+  @moduledoc """
+  The Perfect Memory integration context.
+  """
+
+  require Logger
+
+  def publish_rdf(rdf_content) do
+    config = Application.get_env(:ex_subtil_backend, :perfect_memory_endpoint)
+
+    hostname = System.get_env("PM_ENDPOINT_HOSTNAME") || Keyword.get(config, :hostname, "")
+
+    url = "https://" <> hostname <> "/v1/requests"
+
+    body = %{
+      client_id: Keyword.get(config, :client_id, ""),
+      name: "push_rdf_infos",
+      inputs: %{
+        infos_graph: %{
+          value: rdf_content |> Base.encode64,
+          type: "binary"
+        }
+      }
+    }
+
+    headers = [
+      "Cache-Control": "no-cache",
+      "Content-Type": "application/json",
+      "X-Api-Key": Keyword.get(config, :api_key, ""),
+    ]
+
+    HTTPotion.post(url, [body: body |> Poison.encode!, headers: headers])
+    |> Map.get(:status_code)
+  end
+end
