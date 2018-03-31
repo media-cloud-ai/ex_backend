6644c14ed78f6f5378c6d8a043d3361e476f43c3
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-03-31 19:47:15 +0200

    update selection of new image to start a worker

--- assets/src/app/workers/workers.component.ts
+++ assets/src/app/workers/workers.component.ts
@@ -24,7 +24,6 @@ export class WorkersComponent {
   nodes: NodeConfig[];
   images: Image[];
 
-  selectedNode: NodeConfig;
   selectedWorker: Image;
 
   constructor(
@@ -70,11 +69,10 @@ export class WorkersComponent {
 
   addContainer(): void {
     this.containerService.createContainer(
-      this.selectedNode,
+      this.selectedWorker.node,
       this.selectedWorker.id + "-" + Date.now(),
       this.selectedWorker.params)
     .subscribe(container => {
-      this.selectedNode = undefined;
       this.selectedWorker = undefined;
       this.getContainers();
     });
