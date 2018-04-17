3f1e57a8f66f253c346e9e69cbd406f5d0520d44
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-04-17 09:06:35 +0200

    create user if not exists at the start of the application

--- lib/ex_subtil_backend/application.ex
+++ lib/ex_subtil_backend/application.ex
@@ -82,6 +82,16 @@ defmodule ExSubtilBackend.Application do
       ExSubtilBackend.Migration.AddStatusDescription
     )
 
+    root_email = System.get_env("ROOT_EMAIL") || Application.get_env(:ex_subtil_backend, :root_email)
+    root_password = System.get_env("ROOT_PASSWORD") || Application.get_env(:ex_subtil_backend, :root_password)
+
+    if ExSubtilBackend.Accounts.get_by(%{"email" => root_email}) == nil do
+      user = %{email: root_email, password: root_password}
+
+      {:ok, user} = ExSubtilBackend.Accounts.create_user(user)
+      {:ok, _user} = ExSubtilBackend.Accounts.confirm_user(user)
+    end
+
     main_supervisor
   end
 
