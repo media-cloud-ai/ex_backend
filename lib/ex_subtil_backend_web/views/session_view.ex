a01eab1b3e9b6541159e2fa1659e526bb35d1cd7
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-04-22 15:07:05 +0200

    return user information when opening session

--- lib/ex_subtil_backend_web/views/session_view.ex
+++ lib/ex_subtil_backend_web/views/session_view.ex
@@ -1,7 +1,14 @@
 defmodule ExSubtilBackendWeb.SessionView do
   use ExSubtilBackendWeb, :view
 
-  def render("info.json", %{info: token}) do
-    %{access_token: token}
+  def render("info.json", %{info: token, user: user}) do
+    %{
+      access_token: token,
+      user: %{
+        id: user.id,
+        email: user.email,
+        rights: user.rights,
+      }
+    }
   end
 end
