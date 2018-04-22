12cbedd62ba8e04776df83a38c81b728c35f6b73
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-04-22 15:04:37 +0200

    display related menu to user rights

--- assets/src/app/app.component.ts
+++ assets/src/app/app.component.ts
@@ -13,6 +13,9 @@ import {Subscription}   from 'rxjs/Subscription';
 export class AppComponent {
   loggedIn: boolean;
   menu_opened: boolean = false;
+  right_administrator: boolean;
+  right_technician: boolean;
+  right_editor: boolean;
 
   subIn: Subscription;
   subOut: Subscription;
@@ -24,16 +27,25 @@ export class AppComponent {
     this.subIn = this.authService.userLoggedIn$.subscribe(
       username => {
         this.loggedIn = true;
+        this.right_administrator = this.authService.hasAdministratorRight();
+        this.right_technician = this.authService.hasTechnicianRight();
+        this.right_editor = this.authService.hasEditorRight();
         this.menu_opened = !this.breakpointObserver.isMatched('(max-width: 599px)');
       });
     this.subOut = this.authService.userLoggedOut$.subscribe(
       username => {
         this.loggedIn = false;
         this.menu_opened = false;
+        this.right_administrator = false;
+        this.right_technician = false;
+        this.right_editor = false;
       });
 
     if(this.authService.isLoggedIn) {
       this.loggedIn = true;
+      this.right_administrator = this.authService.hasAdministratorRight();
+      this.right_technician = this.authService.hasTechnicianRight();
+      this.right_editor = this.authService.hasEditorRight();
       this.menu_opened = !this.breakpointObserver.isMatched('(max-width: 599px)');
     }
   }
