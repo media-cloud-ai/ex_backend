f8b12bed1f0851ee16efb07c80c2e16a9469d00a
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-04-24 10:47:10 +0200

    update dashbord with right management

--- assets/src/app/dashboard/dashboard.component.ts
+++ assets/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 
 import {Component} from '@angular/core';
+import {AuthService}    from '../authentication/auth.service';
+import {Subscription}   from 'rxjs/Subscription';
 
 @Component({
     selector: 'dashboard-component',
@@ -7,5 +9,33 @@ import {Component} from '@angular/core';
 })
 
 export class DashboardComponent {
-  constructor() {}
+  right_administrator: boolean;
+  right_technician: boolean;
+  right_editor: boolean;
+
+  subIn: Subscription;
+  subOut: Subscription;
+
+  constructor(public authService: AuthService) {}
+
+  ngOnInit() {
+    this.subIn = this.authService.userLoggedIn$.subscribe(
+      username => {
+        this.right_administrator = this.authService.hasAdministratorRight();
+        this.right_technician = this.authService.hasTechnicianRight();
+        this.right_editor = this.authService.hasEditorRight();
+      });
+    this.subOut = this.authService.userLoggedOut$.subscribe(
+      username => {
+        delete this.right_administrator;
+        delete this.right_technician;
+        delete this.right_editor;
+      });
+
+    if(this.authService.isLoggedIn) {
+      this.right_administrator = this.authService.hasAdministratorRight();
+      this.right_technician = this.authService.hasTechnicianRight();
+      this.right_editor = this.authService.hasEditorRight();
+    }
+  }
 }
