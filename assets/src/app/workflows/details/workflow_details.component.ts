d9a3235f6c6f4bf907c6d3de6ba48ddcb02c9717
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-04-28 17:22:21 +0200

    use Workflow Render and some display updates

--- assets/src/app/workflows/details/workflow_details.component.ts
+++ assets/src/app/workflows/details/workflow_details.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 
 import {WorkflowService} from '../../services/workflow.service';
 import {Workflow, Step} from '../../models/workflow';
+import {WorkflowRender} from '../../models/workflow_render';
 
 @Component({
   selector: 'workflow-details-component',
@@ -13,15 +14,13 @@ export class WorkflowDetailsComponent {
   private sub: any;
 
   workflow: Workflow;
-  graph: Step[][];
+  render: WorkflowRender;
 
   constructor(
     private workflowService: WorkflowService,
     private route: ActivatedRoute,
     private router: Router
-  ) {
-    this.graph = new Array();
-  }
+  ) {}
 
   ngOnInit() {
     this.sub = this.route
@@ -38,9 +37,8 @@ export class WorkflowDetailsComponent {
   getWorkflow(workflow_id): void {
     this.workflowService.getWorkflow(workflow_id)
     .subscribe(workflow => {
-      console.log("Got workflow:", workflow);
       this.workflow = workflow["data"];
-      this.initWorkflowGraph();
+      this.render = new WorkflowRender(this.workflow.flow.steps);
     });
   }
 
@@ -57,93 +55,10 @@ export class WorkflowDetailsComponent {
         count++;
       }
     }
-    return count.toString() + "/" + this.workflow["flow"].steps.length.toString();
+    return count.toString();
   }
 
-  getStepWeight(step: Step): number {
-    let step_line: Step[] = this.graph.find(line => line.includes(step));
-    let step_line_idx: number = this.graph.indexOf(step_line);
-
-    if(step_line.length == 1) {
-      return 1;
-    }
-
-    let children_weigth = 1;
-    let children_line = this.graph[step_line_idx + 1];
-    if(children_line != undefined) {
-      let step_children = children_line.filter(s => s.parent_ids.includes(step.id));
-      children_weigth = 1 / children_line.length;
-      if(step_children.length > 0) {
-        children_weigth = step_children.length / children_line.length;
-      }
-    }
-
-    let parent_weigth = 1;
-    let parent_line = this.graph[step_line_idx - 1];
-    if(parent_line != undefined) {
-      let step_parents = parent_line.filter(s => step.parent_ids.includes(s.id));
-      parent_weigth = 1 / parent_line.length;
-      if(step_parents.length > 0) {
-        parent_weigth = step_parents.length / parent_line.length;
-      }
-    }
-
-    return parent_weigth * children_weigth * step_line.length;
+  getTotalSteps(): number {
+    return this.workflow["flow"].steps.length;
   }
-
-  private initWorkflowGraph(): void {
-
-    for(let step of this.workflow.flow.steps) {
-
-      let child_line_index = 0;
-      if(step.parent_ids.length != 0) {
-
-        let parents_lines_index = this.graph
-          .filter(line => line.filter(s => step.parent_ids.includes(s.id)).length > 0)
-          .map(line => this.graph.indexOf(line));
-        child_line_index = Math.max(...parents_lines_index) + 1;
-      }
-
-      if(this.graph[child_line_index] == undefined) {
-        this.graph[child_line_index] = new Array<Step>();
-      }
-      this.graph[child_line_index].push(step);
-    }
-
-    for (var i = 1; i < this.graph.length; ++i) {
-      let last_line = this.graph[i - 1];
-      let cur_line = this.graph[i];
-
-      let cur_line_length = cur_line.length;
-
-      // we have to ensure each parent has an element under
-      let last_line_ids = last_line.map(s => s.id);
-      last_line_ids = last_line_ids.filter((s, pos) => last_line_ids.indexOf(s) == pos).sort((a, b) => a - b);
-
-      let line_parent_ids = []
-      for(let step of cur_line) {
-        line_parent_ids = line_parent_ids.concat(step.parent_ids);
-      }
-      line_parent_ids = line_parent_ids.filter((s, pos) => line_parent_ids.indexOf(s) == pos).sort((a, b) => a - b);
-
-      let ids_diff = last_line_ids.filter(id => line_parent_ids.indexOf(id) < 0)
-
-      let no_child_parents = last_line.filter(s => ids_diff.includes(s.id));
-      for(let parent of no_child_parents) {
-        let idx = last_line.indexOf(parent);
-        let fake_step = {
-            id: parent.id,
-            parent_ids: parent.parent_ids,
-            name: undefined,
-            enable: true,
-            required: [],
-            parameters: []
-          };
-          cur_line.splice(idx, 0, fake_step);
-      }
-
-      this.graph[i] = cur_line;
-    }
-  }
-
 }
