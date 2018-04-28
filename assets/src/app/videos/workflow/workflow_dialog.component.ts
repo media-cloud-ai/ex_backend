e517bc6437cad8a0dbcb9554134b1378ff8615b4
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-04-28 17:20:17 +0200

    use WorkflowRender

--- assets/src/app/videos/workflow/workflow_dialog.component.ts
+++ assets/src/app/videos/workflow/workflow_dialog.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject} from '@angular/core';
 import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import {Step} from '../../models/workflow';
+import {WorkflowRender} from '../../models/workflow_render';
 
 @Component({
   selector: 'workflow_dialog',
@@ -11,7 +12,7 @@ export class WorkflowDialogComponent {
 
   acs_enable: boolean;
   steps: Step[];
-  graph: Step[][];
+  render: WorkflowRender;
   active_steps = {};
 
   constructor(public dialogRef: MatDialogRef<WorkflowDialogComponent>,
@@ -21,11 +22,10 @@ export class WorkflowDialogComponent {
     this.steps = [
       {
         id: 0,
-        parent_ids: [],
         name: "download_ftp",
         enable: true,
-        required: [],
-        parameters: []
+        parent_ids:[],
+        required: []
       },{
         id: 1,
         parent_ids: [0],
@@ -33,8 +33,7 @@ export class WorkflowDialogComponent {
         enable: true,
         required: [
           "download_ftp"
-        ],
-        parameters: []
+        ]
       },{
         id: 2,
         parent_ids: [0],
@@ -42,8 +41,7 @@ export class WorkflowDialogComponent {
         enable: true,
         required: [
           "download_ftp"
-        ],
-        parameters : []
+        ]
       },{
         id: 3,
         parent_ids: [2],
@@ -51,8 +49,7 @@ export class WorkflowDialogComponent {
         enable: this.acs_enable,
         required: [
           "audio_extraction"
-        ],
-        parameters : []
+        ]
       },{
         id: 4,
         parent_ids: [3],
@@ -60,8 +57,7 @@ export class WorkflowDialogComponent {
         enable: this.acs_enable,
         required: [
           "audio_decode"
-        ],
-        parameters : []
+        ]
       },{
         id: 5,
         parent_ids: [4],
@@ -89,8 +85,7 @@ export class WorkflowDialogComponent {
         enable: true,
         required: [
           "download_http"
-        ],
-        parameters: []
+        ]
       },{
         id: 7,
         parent_ids: [6],
@@ -99,8 +94,7 @@ export class WorkflowDialogComponent {
         required: [
           "audio_extraction",
           "ttml_to_mp4"
-        ],
-        parameters : []
+        ]
       },{
         id: 8,
         parent_ids: [7],
@@ -129,110 +123,20 @@ export class WorkflowDialogComponent {
         enable: true,
         required: [
           "generate_dash"
-        ],
-        parameters: []
+        ]
       },{
         id: 10,
         parent_ids: [9],
         name: "clean_workspace",
+        enable: true,
         required: [
           "download_ftp"
-        ],
-        enable: true,
-        parameters: []
+        ]
       }
     ]
 
-    this.graph = new Array();
-    this.initWorkflowGraph();
-
+    this.render = new WorkflowRender(this.steps);
     this.updateStepRequirements(this.steps[0]);
-  }
-
-  private initWorkflowGraph(): void {
-
-    for(let step of this.steps) {
-
-      let child_line_index = 0;
-      if(step.parent_ids.length != 0) {
-
-        let parents_lines_index = this.graph
-          .filter(line => line.filter(s => step.parent_ids.includes(s.id)).length > 0)
-          .map(line => this.graph.indexOf(line));
-        child_line_index = Math.max(...parents_lines_index) + 1;
-      }
-
-      if(this.graph[child_line_index] == undefined) {
-        this.graph[child_line_index] = new Array<Step>();
-      }
-      this.graph[child_line_index].push(step);
-    }
-
-    for (var i = 1; i < this.graph.length; ++i) {
-      let last_line = this.graph[i - 1];
-      let cur_line = this.graph[i];
-
-      let cur_line_length = cur_line.length;
-
-      // we have to ensure each parent has an element under
-      let last_line_ids = last_line.map(s => s.id);
-      last_line_ids = last_line_ids.filter((s, pos) => last_line_ids.indexOf(s) == pos).sort((a, b) => a - b);
-
-      let line_parent_ids = []
-      for(let step of cur_line) {
-        line_parent_ids = line_parent_ids.concat(step.parent_ids);
-      }
-      line_parent_ids = line_parent_ids.filter((s, pos) => line_parent_ids.indexOf(s) == pos).sort((a, b) => a - b);
-
-      let ids_diff = last_line_ids.filter(id => line_parent_ids.indexOf(id) < 0)
-
-      let no_child_parents = last_line.filter(s => ids_diff.includes(s.id));
-      for(let parent of no_child_parents) {
-        let idx = last_line.indexOf(parent);
-        let fake_step = {
-            id: parent.id,
-            parent_ids: parent.parent_ids,
-            name: undefined,
-            enable: true,
-            required: [],
-            parameters: []
-          };
-          cur_line.splice(idx, 0, fake_step);
-      }
-
-      this.graph[i] = cur_line;
-    }
-  }
-
-  getStepWeight(step: Step): number {
-    let step_line: Step[] = this.graph.find(line => line.includes(step));
-    let step_line_idx: number = this.graph.indexOf(step_line);
-
-    if(step_line.length == 1) {
-      return 1;
-    }
-
-    let children_weigth = 1;
-    let children_line = this.graph[step_line_idx + 1];
-    if(children_line != undefined) {
-      let step_children = children_line.filter(s => s.parent_ids.includes(step.id));
-      children_weigth = 1 / children_line.length;
-      if(step_children.length > 0) {
-        children_weigth = step_children.length / children_line.length;
-      }
-    }
-
-    let parent_weigth = 1;
-    let parent_line = this.graph[step_line_idx - 1];
-    if(parent_line != undefined) {
-      let step_parents = parent_line.filter(s => step.parent_ids.includes(s.id));
-      parent_weigth = 1 / parent_line.length;
-      if(step_parents.length > 0) {
-        parent_weigth = step_parents.length / parent_line.length;
-      }
-    }
-
-    return parent_weigth * children_weigth * step_line.length;
   }
 
   updateStepRequirements(step: Step) {
