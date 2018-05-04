56e86a792889ec2bac4aba418554de97e69a29d5
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-05-04 17:44:36 +0200

    add more information to convert into rdf

--- lib/ex_subtil_backend/rdf/converter.ex
+++ lib/ex_subtil_backend/rdf/converter.ex
@@ -18,10 +18,41 @@ defmodule ExSubtilBackend.Rdf.Converter do
       "qid" => video_id
     }
 
+    workflow =
+      ExSubtilBackend.Workflows.list_workflows(%{video_id: video_id})
+      |> Map.get(:data, [])
+      |> List.first
+
+    manifest =
+      case workflow do
+        nil -> ""
+        _ ->
+          workflow
+          |> Map.get(:artifacts, [])
+          |> List.first
+          |> Map.get(:resources, %{})
+          |> Map.get("manifest")
+      end
+
+    files =
+      ExVideoFactory.get_files_for_video_id(video_id)
+
     information =
       ExVideoFactory.videos(params)
       |> Map.get(:videos)
       |> List.first()
+      |> Map.put(:files, files)
+
+    information =
+      if manifest != "" do
+        manifest =
+          manifest
+          |> String.replace( "/421959/prod/innovation/", "http://videos-pmd.francetv.fr/innovation/")
+        information
+        |> Map.put(:artefacts, %{manifest: manifest})
+      else
+        information
+      end
 
     config = Application.get_env(:ex_subtil_backend, :rdf_converter)
 
