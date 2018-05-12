76426ee34784de38a32c4e89c8757a5cc43d8ff8
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-05-12 08:29:19 +0200

    declare interceptor to check token timeout

---
+++ assets/src/app/authentication/error.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpResponse,
+  HttpInterceptor,
+  HttpErrorResponse,
+} from '@angular/common/http';
+
+import { AuthService } from './auth.service';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(public authService: AuthService) {}
+
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).do(event => {}, err => {
+      if (err instanceof HttpErrorResponse && err.status == 401) {
+        this.authService.logout();
+      }
+    });
+  }
+}
