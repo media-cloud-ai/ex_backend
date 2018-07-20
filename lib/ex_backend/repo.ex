77006fb927df1c9746a38b272fa75b110db5a270
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-07-20 14:51:22 +0200

    be able to configure database via env vars

--- lib/ex_backend/repo.ex
+++ lib/ex_backend/repo.ex
@@ -1,11 +1,53 @@
 defmodule ExBackend.Repo do
   use Ecto.Repo, otp_app: :ex_backend
-
+  require Logger
   @doc """
   Dynamically loads the repository url from the
   DATABASE_URL environment variable.
   """
   def init(_, opts) do
-    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+    case System.get_env("DATABASE_URL") do
+      nil ->
+        opts =
+          case System.get_env("DATABASE_HOSTNAME") do
+            nil -> opts
+            hostname ->
+              Keyword.put(opts, :hostname, hostname)
+          end
+
+        opts =
+          case System.get_env("DATABASE_PORT") do
+            nil -> opts
+            port ->
+              Keyword.put(opts, :port, port)
+          end
+
+        opts =
+          case System.get_env("DATABASE_USERNAME") do
+            nil -> opts
+            username ->
+              Keyword.put(opts, :username, username)
+          end
+
+        opts =
+          case System.get_env("DATABASE_PASSWORD") do
+            nil -> opts
+            password ->
+              Keyword.put(opts, :password, password)
+          end
+
+        opts =
+          case System.get_env("DATABASE_NAME") do
+            nil -> opts
+            database ->
+              Keyword.put(opts, :database, database)
+          end
+
+        Logger.info "connect to #{inspect opts}"
+        {:ok, opts}
+      url ->
+        Logger.info "connect to #{url}"
+        {:ok, Keyword.put(opts, :url, url)}
+    end
   end
 end
