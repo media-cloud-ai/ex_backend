1d251d85c0c3854d760974e8f78f5f4603ad8b49
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-08-17 14:56:16 +0200

    fix include order

--- assets/src/app.ts
+++ assets/src/app.ts
@@ -1,14 +1,13 @@
+require('reflect-metadata')
+require('rxjs')
+require('es7-shim')
+require('zone.js/dist/zone')
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppModule } from './app/app.module'
 import { enableProdMode } from '@angular/core'
 import { environment } from './environments/environment'
 import './theme.scss'
-
-require('reflect-metadata')
-require('rxjs')
-require('es7-shim')
-require('zone.js/dist/zone')
 
 if (environment.production) {
   enableProdMode()
