38dd26d4a97243f05d4a751d563a09b972c58537
Marc-Antoine Arnaud <maarnaud@media-io.com>
2019-02-27 12:22:16 +0100

    add all france télévisions channels

--- assets/src/app/catalog/catalog.component.ts
+++ assets/src/app/catalog/catalog.component.ts
@@ -42,14 +42,64 @@ export class CatalogComponent {
 
   searchInput = ''
   videoid = ''
-  channels = [
-    {id: 'france-2', label: 'France 2'},
-    {id: 'france-3', label: 'France 3'},
-    {id: 'france-4', label: 'France 4'},
-    {id: 'france-5', label: 'France 5'},
-    {id: 'france-o', label: 'France Ô'},
-    {id: 'franceinfo', label: 'France Info'},
+
+  allChannels = [
+    {
+      label: "Premium",
+      list: [
+        {id: 'france-2', label: 'France 2'},
+        {id: 'france-3', label: 'France 3'},
+        {id: 'france-4', label: 'France 4'},
+        {id: 'france-5', label: 'France 5'},
+        {id: 'france-o', label: 'France Ô'},
+        {id: 'franceinfo', label: 'France Info'},
+      ]
+    },
+    {
+      label: "Outre-mer",
+      list: [
+        {id: 'guadeloupe-1ere', label: 'Guadeloupe 1ère'},
+        {id: 'guyane-1ere', label: 'Guyane 1ère'},
+        {id: 'martinique-1ere', label: 'Martinique 1ère'},
+        {id: 'mayotte-1ere', label: 'Mayotte 1ère'},
+        {id: "nouvelle-caledonie-1ere", label: "Nouvelle-Calédonie 1ère"},
+        {id: "polynesie-1ere", label: "Polynésie 1ère"},
+        {id: "reunion-1ere", label: "Réunion 1ère"},
+        {id: "saint-pierre-et-miquelon-1ere", label: "Saint-Pierre et Miquelon 1ère"},
+        {id: "wallis-et-futuna-1ere", label: "Wallis et Futuna 1ère"},
+      ]
+    },
+    {
+      label: "Région",
+      list: [
+        {id: "france-3-alpes", label: "france 3 Alpes"},
+        {id: "france-3-auvergne", label: "france 3 Auvergne"},
+        {id: "france-3-rhone-alpes", label: "france 3 Rhône-Alpes"},
+        {id: "france-3-bourgogne", label: "france 3 Bourgogne"},
+        {id: "france-3-franche-comte", label: "france 3 Franche-Comté"},
+        {id: "france-3-bretagne", label: "france 3 Bretagne"},
+        {id: "france-3-centre-val-de-loire", label: "france 3 Centre-Val de Loire"},
+        {id: "france-3-corse-via-stella", label: "france 3 Corse Via Stella"},
+        {id: "france-3-alsace", label: "france 3 Alsace"},
+        {id: "france-3-lorraine", label: "france 3 Lorraine"},
+        {id: "france-3-champagne-ardenne", label: "france 3 Champagne-Ardenne"},
+        {id: "france-3-nord-pas-de-calais", label: "france 3 Nord Pas-de-Calais"},
+        {id: "france-3-picardie", label: "france 3 Picardie"},
+        {id: "france-3-haute-normandie", label: "france 3 Haute-Normandie"},
+        {id: "france-3-basse-normandie", label: "france 3 Basse-Normandie"},
+        {id: "france-3-limousin", label: "france 3 Limousin"},
+        {id: "france-3-aquitaine", label: "france 3 Aquitaine"},
+        {id: "france-3-poitou-charentes", label: "france 3 Poitou-Charentes"},
+        {id: "france-3-midi-pyrenees", label: "france 3 Midi-Pyrénées"},
+        {id: "france-3-languedoc-roussillon", label: "france 3 Languedoc-Roussillon"},
+        {id: "france-3-paris-ile-de-france", label: "france 3 Paris Ile-de-France"},
+        {id: "france-3-pays-de-la-loire", label: "france 3 Pays de la Loire"},
+        {id: "france-3-provence-alpes", label: "france 3 Provence Alpes"},
+        {id: "france-3-cote-d-azur", label: "france 3 Côte d'Azur"},
+      ]
+    }
   ]
+
   selectedChannels = []
   live = false
   integrale = false
@@ -102,14 +152,6 @@ export class CatalogComponent {
 
   ngOnDestroy() {
     this.sub.unsubscribe()
-  }
-
-  getChannelIDsList(): Array<string> {
-    var channelIds = []
-    this.channels.forEach(function(channel) {
-      channelIds.push(channel.id)
-    })
-    return channelIds
   }
 
   getVideos(index): void {
@@ -246,7 +288,7 @@ export class CatalogComponent {
   start_ftvstudio_ingest(video) {
     this.workflowService.getWorkflowDefinition("ftv_studio_rosetta", video.id)
       .subscribe(workflowDefinition => {
-        workflowDefinition.reference = video.iddsqf
+        workflowDefinition.reference = video.id
         this.workflowService.createWorkflow(workflowDefinition)
           .subscribe(response => {
             let snackBarRef = this.snackBar.open("Rosetta ingest started for \"" + video.title + "\"", "Show workflow", {
