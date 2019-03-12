793f7ad095b23c94c24d7e9e2a875f996d7dffef
Marc-Antoine Arnaud <maarnaud@media-io.com>
2019-03-12 13:53:36 +0100

    filter on wav not mp4

--- lib/ex_backend/workflow/step/acs_synchronize.ex
+++ lib/ex_backend/workflow/step/acs_synchronize.ex
@@ -12,7 +12,7 @@ defmodule ExBackend.Workflow.Step.AcsSynchronize do
 
     case Requirements.get_source_files(workflow.jobs, step) do
       [path1, path2] ->
-        if String.ends_with?(path1, ".mp4") do
+        if String.ends_with?(path1, ".wav") do
           start_processing_synchro(path1, path2, workflow, step, step_id)
         else
           start_processing_synchro(path2, path1, workflow, step, step_id)
