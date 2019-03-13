f5826fbe6c3e34ce73ab3f4d902b257817b68887
Marc-Antoine Arnaud <maarnaud@media-io.com>
2019-03-13 08:06:49 +0100

    update ACS workflow

--- lib/ex_backend/workflow/definition/francetv_subtil_acs.ex
+++ lib/ex_backend/workflow/definition/francetv_subtil_acs.ex
@@ -176,14 +176,27 @@ defmodule ExBackend.Workflow.Definition.FrancetvSubtilAcs do
               enable: true,
               parameters: [
                 %{
-                  id: "order",
-                  type: "string",
-                  value: "publish_dash_and_ttml"
+                  id: "perfect_memory_username",
+                  type: "credential",
+                  default: "PERFECT_MEMORY_USERNAME",
+                  value: "PERFECT_MEMORY_USERNAME"
+                },
+                %{
+                  id: "perfect_memory_password",
+                  type: "credential",
+                  default: "PERFECT_MEMORY_PASSWORD",
+                  value: "PERFECT_MEMORY_PASSWORD"
+                },
+                %{
+                  id: "perfect_memory_endpoint",
+                  type: "credential",
+                  default: "PERFECT_MEMORY_ENDPOINT",
+                  value: "PERFECT_MEMORY_ENDPOINT"
                 },
                 %{
                   id: "perfect_memory_event_name",
                   type: "string",
-                  value: "push_ttml_and_dash"
+                  value: "publish_dash_and_ttml"
                 },
                 %{
                   id: "path",
