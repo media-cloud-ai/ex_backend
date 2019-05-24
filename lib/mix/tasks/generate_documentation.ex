866f30274b50a409ed4f02977182dd8b2063c8c1
Marc-Antoine Arnaud <maarnaud@media-io.com>
2019-05-24 10:51:32 +0200

    update documentation generation

--- lib/mix/tasks/generate_documentation.ex
+++ lib/mix/tasks/generate_documentation.ex
@@ -3,17 +3,18 @@ defmodule Mix.Tasks.GenerateDocumentation do
 
   @shortdoc "Wrote the BlueBird documentation into json file"
   def run(_) do
+    BlueBird.ConnLogger.start_link()
     BlueBird.start()
 
-    {:ok, file} = File.open "documentation.json", [:write]
+    {:ok, file} = File.open("documentation.json", [:write])
 
     documentation =
       BlueBird.Generator.run()
       |> Map.delete(:contact)
       |> Map.delete(:license)
-      |> Poison.encode!
+      |> Poison.encode!()
 
-    IO.binwrite file, documentation
-    File.close file
+    IO.binwrite(file, documentation)
+    File.close(file)
   end
 end
