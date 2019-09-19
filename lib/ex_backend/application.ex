531c683376cb7a5ac604750abaa8c476d656fb7e
Marc-Antoine Arnaud <maarnaud@media-io.com>
2019-09-19 11:08:26 +0200

    start AMQP consumming via a supervisor, move migration in a function

--- lib/ex_backend/application.ex
+++ lib/ex_backend/application.ex
@@ -12,35 +12,18 @@ defmodule ExBackend.Application do
     children = [
       # Start the Ecto repository
       supervisor(ExBackend.Repo, []),
+
+      # Start the AMQP connection supervisor
+      supervisor(ExBackend.Amqp.Supervisor, []),
+
       # Start the endpoint when the application starts
       supervisor(ExBackendWeb.Endpoint, []),
       supervisor(ExBackendWeb.Presence, []),
 
-      # Start your own worker by calling: ExBackend.Worker.start_link(arg1, arg2, arg3)
-      # worker(ExBackend.Worker, [arg1, arg2, arg3]),
+      # Start the AMQP connection to submit messages
       worker(ExBackend.Amqp.Connection, []),
-      # {DynamicSupervisor, name: ExBackend.Amqp.Supervisor, strategy: :one_for_one},
 
-      worker(ExBackend.Amqp.JobAcsCompletedConsumer, []),
-      worker(ExBackend.Amqp.JobAcsErrorConsumer, []),
-      worker(ExBackend.Amqp.JobDashManifestCompletedConsumer, []),
-      worker(ExBackend.Amqp.JobDashManifestErrorConsumer, []),
-      worker(ExBackend.Amqp.JobFFmpegCompletedConsumer, []),
-      worker(ExBackend.Amqp.JobFFmpegErrorConsumer, []),
-      worker(ExBackend.Amqp.JobFileSystemCompletedConsumer, []),
-      worker(ExBackend.Amqp.JobFileSystemErrorConsumer, []),
-      worker(ExBackend.Amqp.JobFtpCompletedConsumer, []),
-      worker(ExBackend.Amqp.JobFtpErrorConsumer, []),
-      worker(ExBackend.Amqp.JobGpacCompletedConsumer, []),
-      worker(ExBackend.Amqp.JobGpacErrorConsumer, []),
-      worker(ExBackend.Amqp.JobHttpCompletedConsumer, []),
-      worker(ExBackend.Amqp.JobHttpErrorConsumer, []),
-      worker(ExBackend.Amqp.JobIsmManifestCompletedConsumer, []),
-      worker(ExBackend.Amqp.JobIsmManifestErrorConsumer, []),
-      worker(ExBackend.Amqp.JobRdfCompletedConsumer, []),
-      worker(ExBackend.Amqp.JobRdfErrorConsumer, []),
-      worker(ExBackend.Amqp.JobSpeechToTextCompletedConsumer, []),
-      worker(ExBackend.Amqp.JobSpeechToTextErrorConsumer, []),
+      # Start workflow manager
       worker(ExBackend.WorkflowStepManager, [])
     ]
 
@@ -49,162 +32,8 @@ defmodule ExBackend.Application do
     opts = [strategy: :one_for_one, name: ExBackend.Supervisor]
     main_supervisor = Supervisor.start_link(children, opts)
 
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_171_116_223_034,
-      ExBackend.Migration.CreateJobs
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_171_121_233_956,
-      ExBackend.Migration.CreateStatus
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_180_213_135_100,
-      ExBackend.Migration.CreateWorkflow
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_180_213_171_900,
-      ExBackend.Migration.AddLinkBetweenJobAndWorkflow
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_180_319_162_700,
-      ExBackend.Migration.CreateArtifacts
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_180_416_110_632,
-      ExBackend.Migration.CreateUsers
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_180_416_094_200,
-      ExBackend.Migration.AddStatusDescription
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_180_421_112_500,
-      ExBackend.Migration.CreatePersons
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_180_421_171_300,
-      ExBackend.Migration.AddUserRight
-    )
-
-    # Ecto.Migrator.up(
-    #   ExBackend.Repo,
-    #   20_180_424_161_800,
-    #   ExBackend.Migration.WorkflowSteps
-    # )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_180_514_190_000,
-      ExBackend.Migration.UpdatePersons
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_180_713_172_000,
-      ExBackend.Migration.CreateNodes
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_180_807_182_800,
-      ExBackend.Migration.CreateWatchers
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_180_831_161_100,
-      ExBackend.Migration.AddCacertFileOnNode
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_180_904_151_104,
-      ExBackend.Migration.AddStepIdOnJob
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_180_904_151_130,
-      ExBackend.Migration.UpdateStepIdOnJob
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_180_910_145_830,
-      ExBackend.Migration.CreateRegistery
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_181_008_122_930,
-      ExBackend.Migration.CreateSubtitles
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_181_008_133_834,
-      ExBackend.Migration.MoveSubtitlesItems
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_181_113_152_855,
-      ExBackend.Migration.CreateCredentials
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_190_226_190_800,
-      ExBackend.Migration.AddFieldsOnWorkflow
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_190_708_132_200,
-      ExBackend.Migration.UpdateCredentialValueLength
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_190_723_091_304,
-      ExBackend.Migration.AddParametersOnWorkflow
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_190_723_153_700,
-      ExBackend.Migration.AddParametersOnJob
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_190_723_161_500,
-      ExBackend.Migration.MoveParamsToParametersOnJob
-    )
-
-    Ecto.Migrator.up(
-      ExBackend.Repo,
-      20_190_723_162_000,
-      ExBackend.Migration.RemoveParamsFromJob
-    )
-
+    ExBackend.Migration.All.apply_migrations()
+    ExBackend.Amqp.Supervisor.start_all_amqp_consumming_connections()
 
     root_email = System.get_env("ROOT_EMAIL") || Application.get_env(:ex_backend, :root_email)
 
---
+++ lib/ex_backend/migrations/all.ex
@@ -0,0 +1,159 @@
+defmodule ExBackend.Migration.All do
+  def apply_migrations() do
+  	Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_171_116_223_034,
+      ExBackend.Migration.CreateJobs
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_171_121_233_956,
+      ExBackend.Migration.CreateStatus
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_180_213_135_100,
+      ExBackend.Migration.CreateWorkflow
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_180_213_171_900,
+      ExBackend.Migration.AddLinkBetweenJobAndWorkflow
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_180_319_162_700,
+      ExBackend.Migration.CreateArtifacts
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_180_416_110_632,
+      ExBackend.Migration.CreateUsers
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_180_416_094_200,
+      ExBackend.Migration.AddStatusDescription
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_180_421_112_500,
+      ExBackend.Migration.CreatePersons
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_180_421_171_300,
+      ExBackend.Migration.AddUserRight
+    )
+
+    # Ecto.Migrator.up(
+    #   ExBackend.Repo,
+    #   20_180_424_161_800,
+    #   ExBackend.Migration.WorkflowSteps
+    # )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_180_514_190_000,
+      ExBackend.Migration.UpdatePersons
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_180_713_172_000,
+      ExBackend.Migration.CreateNodes
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_180_807_182_800,
+      ExBackend.Migration.CreateWatchers
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_180_831_161_100,
+      ExBackend.Migration.AddCacertFileOnNode
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_180_904_151_104,
+      ExBackend.Migration.AddStepIdOnJob
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_180_904_151_130,
+      ExBackend.Migration.UpdateStepIdOnJob
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_180_910_145_830,
+      ExBackend.Migration.CreateRegistery
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_181_008_122_930,
+      ExBackend.Migration.CreateSubtitles
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_181_008_133_834,
+      ExBackend.Migration.MoveSubtitlesItems
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_181_113_152_855,
+      ExBackend.Migration.CreateCredentials
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_190_226_190_800,
+      ExBackend.Migration.AddFieldsOnWorkflow
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_190_708_132_200,
+      ExBackend.Migration.UpdateCredentialValueLength
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_190_723_091_304,
+      ExBackend.Migration.AddParametersOnWorkflow
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_190_723_153_700,
+      ExBackend.Migration.AddParametersOnJob
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_190_723_161_500,
+      ExBackend.Migration.MoveParamsToParametersOnJob
+    )
+
+    Ecto.Migrator.up(
+      ExBackend.Repo,
+      20_190_723_162_000,
+      ExBackend.Migration.RemoveParamsFromJob
+    )
+  end
+end
