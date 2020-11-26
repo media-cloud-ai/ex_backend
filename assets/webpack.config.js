ddd74afc58b800b376dbfdcbfbff8963bcb52f26
Marc-Antoine Arnaud <maarnaud@media-io.com>
2020-11-26 15:22:28 +0100

    update some dependencies

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -28,27 +28,45 @@ const config = {
     rules: [
       {
         test: /\.ts?$/,
-        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+        use: [
+          { loader: 'awesome-typescript-loader' },
+          { loader: 'angular2-template-loader' }
+        ]
       },
       {
         test: /\.css(\?v=\d+\.\d+\.\d+)?$/,
-        loaders: ['style-loader', 'css-loader']
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
       },
       {
         test: /\.less(\?v=\d+\.\d+\.\d+)?$/,
-        loaders: ['to-string-loader', 'css-loader', 'less-loader']
+        use: [
+          { loader: 'to-string-loader' },
+          { loader: 'css-loader' },
+          { loader: 'less-loader' }
+        ]
       },
       {
         test: /\.scss(\?v=\d+\.\d+\.\d+)?$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader']
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ]
       },
       {
         test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file-loader?name=fonts/[name].[ext]'
+        use: [
+          { loader: 'file-loader?name=fonts/[name].[ext]' }
+        ]
       },
       {
         test: /\.html$/,
-        loader: 'raw-loader'
+        use: [
+          { loader: 'raw-loader' }
+        ]
       }
     ]
   },
@@ -58,7 +76,11 @@ const config = {
     }
   },
   plugins: [
-    new CopyWebpackPlugin([{ from: './static' }]),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './static', to: '.' }
+      ]
+    }),
     new webpack.ContextReplacementPlugin(
       /@angular(\\|\/)core(\\|\/)esm5/,
       path.join(__dirname, './assets')
