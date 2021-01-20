cd54e87d94c37a020d604282a626af4379c5597b
Mathia Haure-Touze <mathia.haure-touzet.ext@francetv.fr>
2021-01-20 17:36:30 +0100

    set default log level at info

---
+++ config/test.secret.exs
@@ -0,0 +1,23 @@
+use Mix.Config
+
+# config :step_flow, StepFlow.Amqp,
+#   hostname: "localhost",
+#   port: "5678",
+#   management_port: "15678",
+#   username: "mediacloudai",
+#   password: "mediacloudai",
+#   virtual_host: "media_cloud_ai_dev"
+
+# config :amqp,
+#   hostname: "localhost",
+#   port: "5678",
+#   management_port: "15678",
+#   username: "mediacloudai",
+#   password: "mediacloudai",
+# #   virtual_host: "media_cloud_ai_dev"
+
+# config :step_flow, StepFlow.Metrics.MetricsPlugExporter,
+# path: "/metrics",
+# format: :auto, ## or :protobuf, or :text
+# registry: :default,
+# auth: {:basic, "mediacloudai", "mediacloudai"}
