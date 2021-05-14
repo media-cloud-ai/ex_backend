0e62a326e2468377026114dfe6cec8ef46235bec
Valentin NOEL <valentin.noel@media-io.com>
2021-05-14 15:42:27 +0200

    Display workers status with live workers

--- assets/src/app/models/worker.ts
+++ assets/src/app/models/worker.ts
@@ -2,3 +2,20 @@
 export class Worker {
   id: number
 }
+
+export class WorkerStatus {
+  id: string
+  name: string
+  version: string
+  activity: string
+  current_job?: number
+  job_status?: string
+
+  constructor(id: string, name: string, version: string, activity: string) {
+    this.id = id;
+    this.name = name;
+    this.version = version;
+    this.activity = activity;
+  }
+}
+
