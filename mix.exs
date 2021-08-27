15675ab56a373b1ee83fa22df78b184219ca0398
Nicolas Pierre <nico.pierre@icloud.com>
2021-08-27 12:18:34 +0000

    feat: bump stepflow and bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExBackend.Mixfile do
   def project do
     [
       app: :ex_backend,
-      version: "1.1.0",
+      version: "1.2.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -108,7 +108,7 @@ defmodule ExBackend.Mixfile do
       {:ranch, "~> 1.8.0"},
       {:remote_dockers, "1.4.0"},
       {:sigaws, "~> 0.7.2"},
-      {:step_flow, "1.1.0"},
+      {:step_flow, "1.2.0"},
       {:tesla, "~> 1.4.0"},
       {:timex, "~> 3.6"},
       {:uuid, "~> 1.1"}
