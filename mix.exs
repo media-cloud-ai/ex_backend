8e7690fc6ef7cda154319396c70f0597137d5ce6
Nicolas Pierre <nico.pierre@icloud.com>
2021-10-05 12:32:05 +0000

    bump to 1.3.0-rc1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExBackend.Mixfile do
   def project do
     [
       app: :ex_backend,
-      version: "1.3.0-rc",
+      version: "1.3.0-rc1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
