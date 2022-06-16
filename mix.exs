20e3e23dd315f6854c5e52156d663badea11600d
YannRaphalen <yann5992@gmail.com>
2022-06-16 15:29:43 +0200

    feat: bump to 1.6.0-rc16

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExBackend.Mixfile do
   def project do
     [
       app: :ex_backend,
-      version: "1.6.0-rc15",
+      version: "1.6.0-rc16",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -111,7 +111,7 @@ defmodule ExBackend.Mixfile do
       {:ranch, "~> 1.8.0"},
       {:remote_dockers, "1.4.0"},
       {:sigaws, "~> 0.7.2"},
-      {:step_flow, "1.6.0-rc15"},
+      {:step_flow, "1.6.0-rc16"},
       {:tesla, "~> 1.4.0"},
       {:timex, "~> 3.6"},
       {:uuid, "~> 1.1"}
