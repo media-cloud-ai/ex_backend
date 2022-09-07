479f4b2c1920ca05001724c4041ba6fe4d0abfb8
nipierre <nico.pierre@icloud.com>
2022-09-07 16:06:21 +0200

    fix: allow root to modify its name in ui

--- mix.exs
+++ mix.exs
@@ -112,8 +112,7 @@ defmodule ExBackend.Mixfile do
       {:ranch, "~> 1.8.0"},
       {:remote_dockers, "1.4.0"},
       {:sigaws, "~> 0.7.2"},
-      {:step_flow,
-       git: "https://gitlab.com/media-cloud-ai/backend/ex_step_flow.git", branch: "develop"},
+      {:step_flow, "~> 1.5.0"},
       {:tesla, "~> 1.4.0"},
       {:timex, "~> 3.6"},
       {:uuid, "~> 1.1"}
