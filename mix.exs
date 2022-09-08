ee31cf31a648442c66dd17db9525cc0d9e93dbc9
nipierre <nico.pierre@icloud.com>
2022-09-08 15:01:38 +0200

    feat: bump to 1.6.0-rc22

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExBackend.Mixfile do
   def project do
     [
       app: :ex_backend,
-      version: "1.6.0-rc21",
+      version: "1.6.0-rc22",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
