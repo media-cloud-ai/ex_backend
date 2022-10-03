61f7929419316e340db42b09e3ee1eb275777342
nipierre <nico.pierre@icloud.com>
2022-10-03 10:59:58 +0200

    docs: document backend for swagger doc

--- lib/ex_backend_web/controllers/credential_controller.ex
+++ lib/ex_backend_web/controllers/credential_controller.ex
@@ -1,5 +1,6 @@
 defmodule ExBackendWeb.CredentialController do
   use ExBackendWeb, :controller
+  use PhoenixSwagger
 
   import ExBackendWeb.Authorize
 
@@ -12,13 +13,47 @@ defmodule ExBackendWeb.CredentialController do
   plug(:user_check when action in [:index, :show, :delete])
   plug(:right_administrator_check when action in [:index, :show, :delete])
 
-  api :GET, "/api/credentials" do
-    title("List all Credentials")
-    description("Retrieve all credentials")
+  def swagger_definitions do
+    %{
+      Credential: swagger_schema do
+        title "Credential"
+        description "A credential of MCAI Backend"
+        properties do
+          id :string, "Unique identifier in database"
+          inserted_at :string, "Credential insertion date"
+          key :string, "Credential Key"
+          value :string, "Credential Value"
+        end
+        example %{
+            id: 1,
+            inserted_at: "2022-09-30T15:56:35",
+            key: "key",
+            value: "value"
+        }
+      end,
+      Credentials: swagger_schema do
+        title "Credentials"
+        description "A collection of Credentials"
+        type :array
+        items Schema.ref(:Credential)
+      end
+    }
+  end
 
-    parameter(:page, :integer, optional: true, description: "Index of the page")
-    parameter(:size, :integer, optional: true, description: "Size per page")
-    parameter(:key, :string, description: "Search by key")
+  swagger_path :index do
+    get "/api/credentials"
+    summary "List all credentials"
+    description "Retrieve all credentials"
+    produces "application/json"
+    tag "Credentials"
+    parameters do
+      page :query, :integer, "Index of the page"
+      size :query, :integer, "Size per page"
+      key :query, :string, "Search by key"
+    end
+    security [%{Bearer: []}]
+    response 200, "OK", Schema.ref(:Credentials)
+    response 403, "Unauthorized"
   end
 
   def index(conn, params) do
@@ -38,6 +73,22 @@ defmodule ExBackendWeb.CredentialController do
     render(conn, "index.json", credentials: credentials)
   end
 
+  swagger_path :create do
+    post "/api/credentials"
+    summary "Create credential"
+    description "Create credential"
+    produces "application/json"
+    tag "Credentials"
+    parameters do
+      key :query, :string, "Key"
+      value :query, :string, "Value"
+    end
+    security [%{Bearer: []}]
+    response 201, "Created", Schema.ref(:Credential)
+    response 403, "Unauthorized"
+    response 422, "Unprocessable Entity"
+  end
+
   def create(conn, credential_params) do
     case Credentials.create_credential(credential_params) do
       {:ok, %Credential{} = credential} ->
@@ -52,9 +103,59 @@ defmodule ExBackendWeb.CredentialController do
     end
   end
 
+  swagger_path :show do
+    get "/api/credentials/{id}"
+    summary "Get credential (id)"
+    description "Get a credential by id"
+    produces "application/json"
+    tag "Credentials"
+    operation_id "get_credential_by_id"
+    parameters do
+      id :path, :integer, "Credential ID", required: true
+    end
+    security [%{Bearer: []}]
+    response 200, "OK", Schema.ref(:Credential)
+    response 403, "Unauthorized"
+  end
+
   def show(conn, %{"id" => id}) do
-    credential = Credentials.get_credential_by_key!(id)
+    credential = Credentials.get_credential!(id)
     render(conn, "show.json", credential: credential)
+  end
+
+  swagger_path :get_by_key do
+    get "/api/credentials/search/{key}"
+    summary "Get credential (key)"
+    description "Get a credential by key"
+    produces "application/json"
+    tag "Credentials"
+    operation_id "get_credential_by_key"
+    parameters do
+      key :path, :string, "Credential Key", required: true
+    end
+    security [%{Bearer: []}]
+    response 200, "OK", Schema.ref(:Credential)
+    response 403, "Unauthorized"
+  end
+
+  def get_by_key(conn, %{"key" => key}) do
+    credential = Credentials.get_credential_by_key!(key)
+    render(conn, "show.json", credential: credential)
+  end
+
+  swagger_path :delete do
+    PhoenixSwagger.Path.delete "/api/credentials/{id}"
+    summary "Delete credential"
+    description "Delete credential by id"
+    produces "application/json"
+    tag "Credentials"
+    operation_id "delete_user"
+    parameters do
+      id :path, :integer, "Credential ID", required: true
+    end
+    security [%{Bearer: []}]
+    response 204, "No Content"
+    response 403, "Unauthorized"
   end
 
   def delete(conn, %{"id" => id}) do
