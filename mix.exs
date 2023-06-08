77ac849939631e27b01aa5d18dec651f067f4ed6
Thibaud Le Graverend <thibaud.legraverend@media-io.com>
2023-06-08 15:00:33 +0200

    chores: bump to 1.7.3-rc0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExBackend.Mixfile do
   def project do
     [
       app: :ex_backend,
-      version: "1.7.2",
+      version: "1.7.3-rc0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
