35ce8e47edb85af39a93e82b98c617d0de22c280
Valentin NOEL <valentin.noel@luminvent.com>
2024-04-23 11:56:22 +0200

    fix: remove useless company related fields from application endpoint response

--- assets/src/app/models/application.ts
+++ assets/src/app/models/application.ts
@@ -1,6 +1,4 @@
 export class Application {
-  company: string
-  company_logo: string
   identifier: string
   label: string
   logo: string
--- lib/ex_backend_web/controllers/open_api_schemas/application.ex
+++ lib/ex_backend_web/controllers/open_api_schemas/application.ex
@@ -12,16 +12,12 @@ defmodule ExBackendWeb.OpenApiSchemas.Application do
       description: "A MCAI Backend application description",
       type: :object,
       properties: %{
-        company: %Schema{type: :string, description: "Company name"},
-        company_logo: %Schema{type: :string, description: "Company logo"},
         identifier: %Schema{type: :string, description: "Application name"},
         label: %Schema{type: :string, description: "Application label"},
         logo: %Schema{type: :string, description: "Application logo"},
         version: %Schema{type: :string, description: "Application version"}
       },
       example: %{
-        "company" => "MyCompany",
-        "company_logo" => "/bundles/images/logo_id_2018_fushia13_2lignes.png",
         "identifier" => "MCAI BACKEND",
         "label" => "Backend",
         "logo" => "/bundles/images/app_logo.png",
