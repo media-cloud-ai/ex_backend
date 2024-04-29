6cb6a53fd5bc1c720e2b21dc7e1680fa9c498068
nipierre <nico.pierre@icloud.com>
2024-04-29 14:29:45 +0200

    build: bump version to 1.8.1-rc4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExBackend.Mixfile do
   def project do
     [
       app: :ex_backend,
-      version: "1.8.1-rc3",
+      version: "1.8.1-rc4",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
