2f1983c6a6c77134d78644001908ee763a755133
nipierre <nico.pierre@icloud.com>
2025-08-26 14:03:58 +0200

    wip

--- config/prod.exs
+++ config/prod.exs
@@ -135,10 +135,10 @@ config :step_flow, StepFlow.Amqp,
   server_configuration: {:system, "AMQP_SERVER_CONFIGURATION", "standalone"},
   max_retry_to_timeout: {:system, "AMQP_MAX_RETRIES_TO_TIMEOUT"}
 
-config :step_flow, StepFlow.WorkflowDefinitions.ExternalLoader, specification_folder: "./static/"
+config :step_flow, StepFlow.WorkflowDefinitions.ExternalLoader, specification_folder: "/app"
 
 config :step_flow, StepFlow.WorkflowDefinitions.WorkflowDefinition,
-  workflow_schema_url: "./static/standard/1.13/workflow-definition.schema.json"
+  workflow_schema_url: "/app/standard/1.13/workflow-definition.schema.json"
 
 config :ex_backend, :pow_assent,
   providers: [
