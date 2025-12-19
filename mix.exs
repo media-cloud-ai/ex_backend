b35f704fe8015584121a4aaa56b85c753d3b86ab
nipierre <nico.pierre@icloud.com>
2025-12-19 18:33:07 +0100

    chores: update to 1.9.0-rc16

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExBackend.Mixfile do
   def project do
     [
       app: :ex_backend,
-      version: "1.9.0-rc15",
+      version: "1.9.0-rc16",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
