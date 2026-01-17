0fd40c819de74b70284d00596000329058625478
Nicolas Pierre <nico.pierre@icloud.com>
2026-01-17 11:42:42 +0100

    ci: test buildah

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExBackend.Mixfile do
   def project do
     [
       app: :ex_backend,
-      version: "1.9.0-rc18",
+      version: "1.9.0-rc19-dev0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
